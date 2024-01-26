8d8487c3109254120a0b93143dc078d2216f8d99
Marco Andrea <crazyelectron1@gmail.com>
2024-01-26 23:53:35 +0300

    Update launch_chroot.sh
    Optimized mount_fs by adding noatime option for improved performance.
    Introduced tmpfs mounts in mount_kindle_system to reduce writes and enhance speed.
    Added network configuration file copying in setup_resolv for completeness.
    Enhanced unmount_kindle_system with additional umount commands for specific paths.
    In _unmount, forcefully kill processes using the chroot environment before unmounting.

--- kindle/launch_chroot.sh
+++ kindle/launch_chroot.sh
@@ -2,15 +2,20 @@
 MNT_PATH="/tmp/alpine"
 
 create_mountpoint() {
-  if [ ! -d $MNT_PATH ]; then
-    mkdir $MNT_PATH
-  fi
+  mkdir -p $MNT_PATH
 }
 mount_fs() {
-  mount -o loop /mnt/us/alpine-chroot/alpine.ext3 $MNT_PATH
+  mount -o loop,noatime /mnt/us/alpine-chroot/alpine.ext3 $MNT_PATH
 }
 
 mount_kindle_system() {
+  mount -o mode=1777,nosuid,nodev -t tmpfs tmp $MNT_PATH/tmp
+  #mount -o mode=0755,nosuid,nodev -t tmpfs run $MNT_PATH/run
+
+  mount -o bind $MNT_PATH/tmp $MNT_PATH/var/cache
+  mount -o bind $MNT_PATH/tmp $MNT_PATH/var/log
+  mount -o bind $MNT_PATH/tmp $MNT_PATH/run
+
   mount -o bind /proc $MNT_PATH/proc
   mount -o bind /sys $MNT_PATH/sys
   mount -o bind /dev $MNT_PATH/dev
@@ -18,19 +23,31 @@ mount_kindle_system() {
 }
 
 setup_resolv() {
-  rm $MNT_PATH/etc/resolv.conf
-  cp /etc/resolv.conf $MNT_PATH/etc/resolv.conf
+  cp -f /etc/resolv.conf $MNT_PATH/etc/resolv.conf
+  cp -f /etc/network/interfaces	$MNT_PATH/etc/network/interfaces
 }
 
 unmount_kindle_system() {
+  umount $MNT_PATH/sys/kernel/debug/tracing
+  umount $MNT_PATH/sys/kernel/debug
+  umount $MNT_PATH/sys/kernel/config
+  umount $MNT_PATH/sys/fs/fuse/connections
+  umount $MNT_PATH/sys/fs/selinux
+  umount $MNT_PATH/sys/fs/pstore
+  umount $MNT_PATH/dev/shm
+  
   umount $MNT_PATH/dev/pts/ 
   umount $MNT_PATH/dev 
   umount $MNT_PATH/sys 
   umount $MNT_PATH/proc
+
+  umount $MNT_PATH/tmp
 }
 
 unmount_alpine_mount() {
+  LOOPDEV="$(mount | grep loop | grep $MNT_PATH | cut -d" " -f1)"
   umount $MNT_PATH
+  losetup -d $LOOPDEV
 }
 
 _mount() {
@@ -41,6 +58,7 @@ _mount() {
 }
 
 _unmount() {
+  kill -9 $(lsof -t +D $MNT_PATH)
   unmount_kindle_system
   unmount_alpine_mount
 }
